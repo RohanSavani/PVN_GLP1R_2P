7170c8b3e6c696254764e8f0091b7b7bbbcc2ae9
Rohan Savani <savanirh@gmail.com>
2025-05-06 15:31:37 -0400

    pylance prefs, new viz and analysis functions

---
+++ src/scripts/process_2p.py
@@ -0,0 +1,101 @@
+import os
+import numpy as np
+from scipy.ndimage import uniform_filter1d
+
+
+def load_s2p_data(path_to_suite2p_folder):
+    """
+    Load 2p data from the specified path.
+    """
+    # Load the data
+    f = np.load(os.path.join(path_to_suite2p_folder, 'F.npy'))
+    fn = np.load(os.path.join(path_to_suite2p_folder, "Fneu.npy"))
+    iscell=np.load(os.path.join(path_to_suite2p_folder,"iscell.npy"))
+    ops=np.load(os.path.join(path_to_suite2p_folder,"ops.npy"), allow_pickle=True)
+
+
+    return f, fn, iscell, ops
+
+
+def filter_cells(F, iscell, filter_by='first'):
+    """
+    Filters the cells in F based on the iscell array.
+    
+    Parameters:
+        F (np.ndarray): Fluorescence data array.
+        iscell (np.ndarray): Array indicating cell classification.
+        filter_by (str): 'first' to filter by iscell[:, 0] == 1,
+                        'second' to filter by iscell[:, 1] == 1.
+    Returns:
+        np.ndarray: Filtered fluorescence data. 
+    """
+    if filter_by == 'first':
+        mask = iscell[:, 0] == 1
+    elif filter_by == 'second':
+        mask = iscell[:, 1] == 1
+    else:
+        raise ValueError("filter_by must be 'first' or 'second'")
+    return F[mask]
+
+
+def rearrange_data(F, n_trials = 30, framespertrial = 375, fps = 15):
+    """
+    Rearranges the fluorescence data into a 3D array.
+    
+    Parameters:
+        F (np.ndarray): Fluorescence data array.
+        n_trials (int): Number of trials.
+        framespertrial (int): Number of frames per trial.
+        fps (int): Frames per second.
+        
+    Returns:
+        np.ndarray: Rearranged fluorescence data.
+    """
+    n_cells = F.shape[0]
+
+    # Reshape F to have dimensions (n_cells, n_trials, framespertrial)
+    F_reshaped = F.reshape(n_cells, n_trials, framespertrial)
+    
+    return F_reshaped
+
+
+def normalize_data(F, use_baseline = True, baseline_period = [0, 105], trial_by_trial = True):
+    """
+    Normalizes the fluorescence data.
+    
+    Parameters:
+        F (np.ndarray): Fluorescence data array.
+        use_baseline (bool): Whether to use baseline normalization.
+        baseline_period (list): Time period for baseline normalization.
+        
+    Returns:
+        np.ndarray: Normalized fluorescence data.
+    """
+    n_cells, n_trials, framespertrial = F.shape
+    F_normalized = np.zeros_like(F)
+
+    if trial_by_trial:
+        for i in range(n_cells):
+            for j in range(n_trials):
+                if use_baseline:
+                    baseline = np.mean(F[i, j, baseline_period[0]:baseline_period[1]])
+                else:
+                    baseline = np.mean(F[i, j, :])
+            
+                F_normalized[i, j, :] = (F[i, j, :]) / baseline
+        
+    return F_normalized 
+
+
+def moving_average(data, window_size = 5):
+    """
+    Computes the moving average of the data.
+    
+    Parameters:
+        data (np.ndarray): Data to compute the moving average on.
+        window_size (int): Size of the moving window.
+        
+    Returns:
+        np.ndarray: Moving average of the data.
+    """
+    return uniform_filter1d(data, size=window_size)
